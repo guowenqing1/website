8d9aaf7a6714035858c4a47057a20e7e04b043b8
过文庆 <guo_web@qq.com>
2024-06-21 17:42:27 +0800

    1

--- src/components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import "./sections/loader.css";
-export function Loading() {
-  return (
-    <span className="fixed flex justify-center items-center z-[70] top-0 left-0 w-full h-full bg-slate-700 opacity-70">
-      <div className="loader"></div>
-    </span>
-  );
-}
